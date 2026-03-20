ab69730a5b81333f43e060fcac87083baa97c5a3
root <root@localhost.localdomain>
2026-03-20 21:01:39 +0800

    first commit

---
+++ mc1.20/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /root/mc1.20
+echo "eula=true"
+java -Xms1G -Xmx2G -jar /root/mc1.20/spigot-1.20.6.jar nogui
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker load -i CentOS_7.9.2009.tar && \
+docker build -t mc-service:v1.20.6 -f Dockerfile-mc . && \
+docker run -d --name mc-service-1.20.6 --restart always -p 25565:25565 mc-service:v1.20.6
