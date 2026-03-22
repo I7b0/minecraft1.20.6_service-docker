5375a74c51d7ce13dc4d134cb06a4885dbf6543a
I7b0 <2120099347@qq.com>
2026-03-22 14:34:29 +0800

    Fix docker load command in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker load -i CentOS_7.9.2009.tar && \
+bash docker_install.sh
 docker build -t mc-service:v1.20.6 -f Dockerfile-mc . && \
 docker run -d --name mc-service-1.20.6 --restart always -p 25565:25565 mc-service:v1.20.6
