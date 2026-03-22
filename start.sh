c7072ae68def0f3afc6f64c7d12cee09a58e1692
I7b0 <2120099347@qq.com>
2026-03-22 15:48:14 +0800

    Add execute permission for docker_install.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chmod +x docker_install.sh
 sudo bash docker_install.sh
 sudo docker build -t mc-service:v1.20.6 -f Dockerfile-mc . && \
 sudo docker run -d --name mc-service-1.20.6 --restart always -p 25565:25565 mc-service:v1.20.6
