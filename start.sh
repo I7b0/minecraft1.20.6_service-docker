e5cf531d5f8daa3eafba0319a15c071d9fcc3815
I7b0 <2120099347@qq.com>
2026-03-22 14:36:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bash docker_install.sh
-docker build -t mc-service:v1.20.6 -f Dockerfile-mc . && \
-docker run -d --name mc-service-1.20.6 --restart always -p 25565:25565 mc-service:v1.20.6
+sudo bash docker_install.sh
+sudo docker build -t mc-service:v1.20.6 -f Dockerfile-mc . && \
+sudo docker run -d --name mc-service-1.20.6 --restart always -p 25565:25565 mc-service:v1.20.6
